a123b872b7d52a4f62cfb82b65cb2916824b2cc9
Raiondesu <a.raiondesu@gmail.com>
2018-09-15 18:39:58 +0300

    Add BaseBound documentation.

--- src/bound/base.ts
+++ src/bound/base.ts
@@ -14,10 +14,31 @@ export interface IBoundAction<T extends object> {
 export type IBoundPlugin<T extends object> = (action: IBoundAction<T>) => void;
 
 export default abstract class BaseBound<T extends object> {
+  /**
+   * Stores bindings in a structure that is identical to the binding-prototype-object.
+   */
   public storage: IBindingStorage<T> = {} as any;
+
+  /**
+   * A bound object created from a constuctor's snapshot object.
+   *
+   * Contains an instance of the Bound class itself by the `__bound__` key.
+   */
   public boundObject = { __bound__: this } as T & { __bound__: BaseBound<T> };
 
+  /**
+   * Creates an instance of Bound.
+   * @param obj used as an object snapshot for the creation of boundObject and storage. Doesn't become bound itself.
+   * @param [plugins] to plug into the binding events.
+   */
   public constructor(obj: T, public readonly plugins?: IBoundPlugin<T>[]) {
+    // Make __bound__ non-enumerable.
+    Object.defineProperty(this, 'boundObject', {
+      value: { __bound__: this },
+      enumerable: false,
+      writable: false
+    });
+
     if (BaseBound.config.debug && typeof obj !== 'object') {
       throw new BoundError('Only object binds are allowed. For property and pure value bindings use Binding from "bound/binding".');
     }
@@ -27,10 +48,33 @@ export default abstract class BaseBound<T extends object> {
     }
   }
 
+  /**
+   * Binds an object to all other current subscribers
+   *
+   * @template U used to capture the bound object type. Must extends original template type.
+   * @param obj to bind
+   * @param [twoWay] whether the binding should be two-way
+   */
   public abstract bind<U extends T>(obj: U, twoWay?: boolean);
+
+  /**
+   * Unbinds an object and destroys all of its listeners
+   *
+   * @param obj reference of object to be unbound
+   */
   public abstract unbind<U extends T>(obj: U);
 
+  /**
+   * Global binding config. Changes affect all instances.
+   */
   public static get config() { return config; }
+
+
+  /**
+   * Checks whether an object is already bound.
+   *
+   * @param obj an object ot check
+   */
   public static isBound(obj: any) {
     return !!obj.__bound__ && (obj.__bound__ instanceof BaseBound);
   }
